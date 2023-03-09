02f09b346991c32fdb98f2593c5a54306c8a8a02
Oguzcan <Oguzcan.avci95@gmail.com>
2023-03-09 10:27:14 +0100

    Skapat ett json objekt.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import org.json.simple.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello");
+
+        //SKapa JSON object
+        JSONObject jsonOb  = new JSONObject();
+
+        //Spara värden i JSON object
+        jsonOb.put("namn", "Ozzi");
+        jsonOb.put("age", "28");
+
+        //Skriva ut värden
+        System.out.println(jsonOb.get("namn"));
+        System.out.println("I am " + jsonOb.get("age") +  " years old");
+
+        System.out.println("---------");
+        System.out.println();
+
+
+
     }
 }
